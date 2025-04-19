28016e7ddcca4de3c65637f7003f75e9256624a9
nathanhollows <me@nathanhollows.com>
2025-04-19 15:44:44 +1200

    refactor: simplify and rename home.go -> play.go

--- internal/handlers/players/play.go
+++ internal/handlers/players/play.go
@@ -3,57 +3,23 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/nathanhollows/Rapua/v3/internal/sessions"
 	templates "github.com/nathanhollows/Rapua/v3/internal/templates/players"
 	"github.com/nathanhollows/Rapua/v3/models"
 )
 
 // Play shows the player the first page of the game.
 func (h *PlayerHandler) Play(w http.ResponseWriter, r *http.Request) {
-	session, _ := sessions.Get(r, "scanscout")
-	teamCode := session.Values["team"]
-	var team *models.Team
-	var err error
-
-	// If the team is already playing, redirect to the next page
-	if teamCode != nil {
-		team, err = h.GameplayService.GetTeamByCode(r.Context(), teamCode.(string))
-		if err == nil {
-			if r.Header.Get("HX-Request") == "true" {
-				w.Header().Set("HX-Redirect", "/next")
-			} else {
-				http.Redirect(w, r, "/next", http.StatusFound)
-			}
-			return
-		} else {
-			h.Logger.Error("Home get team from session code", "err", err, "team", teamCode)
-			session.Options.MaxAge = -1
-			err := session.Save(r, w)
-			if err != nil {
-				h.handleError(w, r,
-					"Home: saving session", "Error saving session. Please try again.", "error", err)
-				return
-			}
-		}
-	}
+	team, _ := h.getTeamFromContext(r.Context())
 
 	if team == nil {
 		team = &models.Team{}
 	}
+
 	c := templates.Home(*team)
-	err = templates.Layout(c, "Home", nil).Render(r.Context(), w)
+	err := templates.Layout(c, "Home", nil).Render(r.Context(), w)
 	if err != nil {
 		h.Logger.Error("Home: rendering template", "error", err)
 	}
-
-	// Destroy the session now
-	session.Options.MaxAge = -1
-	err = session.Save(r, w)
-	if err != nil {
-		h.handleError(w, r, "Home: saving session", "Error saving session. Please try again.", "error", err)
-		return
-	}
-
 }
 
 // PlayPost is the handler for the play form submission.
@@ -84,5 +50,5 @@ func (h *PlayerHandler) PlayPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("HX-Redirect", "/next")
+	h.redirect(w, r, "/next")
 }
