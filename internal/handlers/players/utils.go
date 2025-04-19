ca8e6748fcfb568b1b0a982e877d29b1144a1e08
nathanhollows <me@nathanhollows.com>
2025-04-19 17:37:36 +1200

    style: remove unused function

--- internal/handlers/players/utils.go
+++ internal/handlers/players/utils.go
@@ -39,14 +39,6 @@ func NewPlayerHandler(
 	}
 }
 
-// getTeamIfExists retrieves a team by its code if present.
-func (h *PlayerHandler) getTeamIfExists(ctx context.Context, teamCode interface{}) (*models.Team, error) {
-	if teamCode == nil {
-		return nil, nil
-	}
-	return h.GameplayService.GetTeamByCode(ctx, teamCode.(string))
-}
-
 // GetTeamFromContext retrieves the team from the context.
 // Team will always be in the context because the middleware.
 // However the Team could be nil if the team was not found.
