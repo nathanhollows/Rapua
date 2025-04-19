3f188bc3f14d9b4e294e64a87d77dffc69a3030e
nathanhollows <me@nathanhollows.com>
2025-04-19 15:38:41 +1200

    fix: linking form, js to close modal on button

--- internal/templates/players/lobby_templ.go
+++ internal/templates/players/lobby_templ.go
@@ -248,7 +248,7 @@ func instructions(instance models.InstanceSettings) templ.Component {
 			var templ_7745c5c3_Var10 string
 			templ_7745c5c3_Var10, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprint(instance.MaxNextLocations))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/players/lobby.templ`, Line: 193, Col: 73}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/players/lobby.templ`, Line: 198, Col: 73}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var10))
 			if templ_7745c5c3_Err != nil {
