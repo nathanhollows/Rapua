61965e1b16133f1fb78e3a0d4df3b5394075de7d
nathanhollows <me@nathanhollows.com>
2025-09-27 20:19:20 +1200

    style: new logo

--- blocks/clue_block.go
+++ blocks/clue_block.go
@@ -29,7 +29,7 @@ func (b *ClueBlock) GetDescription() string {
 func (b *ClueBlock) GetOrder() int  { return b.Order }
 func (b *ClueBlock) GetPoints() int { return b.Points }
 func (b *ClueBlock) GetIconSVG() string {
-	return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lightbulb"><path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5"/><path d="M9 18h6"/><path d="M10 22h4"/></svg>`
+	return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>`
 }
 func (b *ClueBlock) GetData() json.RawMessage {
 	data, _ := json.Marshal(b)
