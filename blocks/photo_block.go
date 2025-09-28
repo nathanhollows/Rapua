82dac1a1c14951c742c7df241f4e22053e39332b
nathanhollows <me@nathanhollows.com>
2025-09-28 16:55:53 +1300

    chore: remove code smell

--- blocks/photo_block.go
+++ blocks/photo_block.go
@@ -31,7 +31,7 @@ func (b *PhotoBlock) GetPoints() int { return b.Points }
 func (b *PhotoBlock) GetIconSVG() string {
 	return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-camera"><path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"/><circle cx="12" cy="13" r="3"/></svg>`
 }
-func (b *PhotoBlock) GetAdminData() interface{} {
+func (b *PhotoBlock) GetAdminData() any {
 	return &b
 }
 func (b *PhotoBlock) GetData() json.RawMessage {
@@ -67,7 +67,7 @@ func (b *PhotoBlock) UpdateBlockData(input map[string][]string) error {
 func (b *PhotoBlock) RequiresValidation() bool { return true }
 
 func (b *PhotoBlock) ValidatePlayerInput(state PlayerState, input map[string][]string) (PlayerState, error) {
-	if input["url"] == nil || len(input["url"]) == 0 {
+	if len(input["url"]) == 0 {
 		return state, errors.New("photo is a required field")
 	}
 
